ffb3476f03e392d079f3a3a93f17403fbd99be31
Diego A. Castro <castrito@gmail.com>
2014-06-08 16:27:49 -0400

    twitter auth done

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['1452788382-xdS7HJtMwu7G5chgPB6NyOudzfs9ZAOzaHXzxPQ'], ENV['JwDtgh8Ucmo6DoZeXtcrYEiluvm1Qn2BEB91HPhTo']
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+  provider :twitter, 'b1BcFmbc1ILHAcVbhNKyg', 'T7JUvNcTu3RvJ12RRmRkdnaccpH8RDmrZwFR2AY'
+  provider :facebook, 'FACEBOOK_ID', 'FACEBOOK_SECRET'
 end
