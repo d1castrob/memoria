3d0a4451b9d3cb539b08d312bc2ebab5a24c24d8
Diego A. Castro <castrito@gmail.com>
2014-06-08 16:40:44 -0400

    auth fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'b1BcFmbc1ILHAcVbhNKyg', 'T7JUvNcTu3RvJ12RRmRkdnaccpH8RDmrZwFR2AY'
-  provider :facebook, 'FACEBOOK_ID', 'FACEBOOK_SECRET'
+  provider :facebook, '625306310826804', '166ce5139e058e48c51328bcfb61eaa6'
 end
