229e8bea6437ccc128c4cd4cd6616c3a561dd52e
Diego A. Castro <castrito@gmail.com>
2014-07-11 14:43:21 -0400

    soporte csv

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,17 @@
 class WelcomeController < ApplicationController
 
 
-def index
+# def index
 	
+# end
+
+def index
+  respond_to do |format|
+  	format.html
+    format.csv { send_data @messages.to_csv }
+    #format.csv { render text:  @messages.to_csv }
+  end
 end
+
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,13 @@
 class Message < ActiveRecord::Base
+
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |message|
+        csv << message.attributes.values_at(*column_names)
+      end
+    end
+  end
+
 end
