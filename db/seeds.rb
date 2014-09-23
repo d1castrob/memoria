60280969696f544019c576bc0a9ef7332593a1e5
Diego A. Castro <castrito@gmail.com>
2014-09-23 16:36:03 -0400

    cambios para limpieza en visualizacion

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,18 @@ ActiveRecord::Base.logger = nil
 #para cada linea del archivo
 file.each_line do |line|
   # para el mismo evento
-  if line.split(/\t/)[15].remove(/\n/) == '31526'
+  if line.split(/\t/)[15].remove(/\n/) == '26900'
+    puts 'found'
     # likes = rt, comments = favs
     l = line.split(/\t/)
-    Message.create(from: l[12], text: l[1], id_at_site:l[0], likes:l[3], comments: l[9], created_at: l[14].to_datetime)
+   	m = Message.find_by_text(l[1])
+   	if m.nil?
+      Message.create(from: l[12], text: l[1], id_at_site:l[0], likes:l[3], comments: l[9], created_at: l[14].to_datetime)
+  	else
+  	  #add favorites and retweets
+  	  m.likes = m.likes + l[3].to_i
+  	  m.comments = m.likes + l[9].to_i
+      m.save
+  	end
   end
 end
