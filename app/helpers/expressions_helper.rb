bd4d30831990978095f83ff065670c00d677b519
Diego A. Castro <castrito@gmail.com>
2014-12-22 12:54:19 -0300

    refactoring grafico semantico

--- app/helpers/expressions_helper.rb
+++ app/helpers/expressions_helper.rb
@@ -1,6 +1,7 @@
 module ExpressionsHelper
 
 def semantic_graph_data
+    ActiveRecord::Base.logger = nil
 
 	@node_rows = []
 	@link_rows = []
@@ -21,5 +22,30 @@ def semantic_graph_data
 
 	@data = { :nodes => @node_rows, :links => @link_rows }
 end
+
+#to be tested, solo si el ID concuerda con el orden en el cual se meten los datos
+def semantic_graph_data2
+
+    ActiveRecord::Base.logger = nil
+
+	@node_rows = []
+	@link_rows = []
+
+	Expression.all.each_with_index do |u1|
+		node_row = {:name => u1.raw_text , :group => 1, :mentions => 2*Math.sqrt(u1.count) }
+		@node_rows << node_row
+
+		u1.relationships.each do |e|
+			if !e.blank?
+				link_row = {:source => u1.id, :target => e.id, :value => e.count}
+				@link_rows << link_row
+			end
+		end
+	end
+
+	@data = { :nodes => @node_rows, :links => @link_rows }
+end
+
+
 
 end
